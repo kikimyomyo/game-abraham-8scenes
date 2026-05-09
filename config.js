3bd2ada9b961c5a3ca125f94f96cb300370f57bf
kikimyomyo <request1004@gmail.com>
2026-05-09 11:30:36 +0900

    deploy: abraham-8scenes

---
+++ config.js
@@ -0,0 +1,6 @@
+window.GAME_CONFIG = {
+  game: "abraham-8scenes",
+  title: "아브라함 8장면",
+  sheet_tab: "abraham-8scenes",
+  endpoint: "https://script.google.com/macros/s/AKfycbx3a96h-vRywYTCxOJxdn199uaFAELyfzy7LRIGeInFuG-ZhxsBh_AKso--Yw8PbYPH/exec",
+};
